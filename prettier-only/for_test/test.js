8186317e72868d4b6fbdd336e0a5163a3530662b
Yasuo Fukuda <yasuo.fukuda@theport.jp>
2019-07-19 16:31:04 +0900

    test

--- prettier-only/for_test/test.js
+++ prettier-only/for_test/test.js
@@ -10,4 +10,23 @@ function hoge() {
     return false
 }
 
+// js hado-ken
+getData(function (a) {
+  getMoreData(a, function (b) {
+    getMoreData(b, function (c) {
+      getMoreData(c, function (d) {
+        getMoreData(d, function (e) {
+          console.log(e);
+        });
+      });
+    });
+  });
+});
+function getData(hoge) {
+  return hoge;
+}
+function getMoreData(hoge) {
+  return hoge++;
+}
+
 console.log(foo)

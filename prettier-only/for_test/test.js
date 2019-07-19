f6f111d03096b024c2c96feb367990c29a8ee07b
Yasuo Fukuda <yasuo.fukuda@theport.jp>
2019-07-19 19:23:09 +0900

    test for string literal

--- prettier-only/for_test/test.js
+++ prettier-only/for_test/test.js
@@ -58,5 +58,5 @@ var widgetStructure = `<div id="${widgetID}">` +
                         '</div>';
 
 // long html strings
-var quote-and-plus   = '<div id="' + widgetContentID + '" style="' + widgetContentStyles + '">' + '</div>'
-var template-literal = `<div id="${widgetContentID}" style="${widgetContentStyles}"></div>`
+var quote-and-plus   = '<div id="' + widgetContentID + '" style="' + widgetContentStyles + '">' + '</div>';
+var template-literal = `<div id="${widgetContentID}" style="${widgetContentStyles}"></div>`;
